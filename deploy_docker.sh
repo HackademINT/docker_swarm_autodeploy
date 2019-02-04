8ac3ef6c90cb3c35a5f10f2cf3b91f7c87cc16b7
Nicolas Bonnet <nicolas.bonnet@telecom-sudparis.eu>
2019-02-04 21:12:08 +0100

    Change IP

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -2,14 +2,14 @@
 
 i=1
 VM_ID="100$i"
-VM_IP="10.20.20.1$i"
+VM_IP="10.33.0.1$i"
 if [ "$(qm list | grep $VM_ID | wc -l)" -eq 0 ]; then
 	qm clone 9000 $VM_ID \
 		--name "docker$i" \
 		--description 'Docker $i VM created by bonnetn' \
 		--pool dockerswarm 
-	qm set $VM_ID --net0 model=virtio,bridge=vmbr20
-	qm set $VM_ID --ipconfig0 ip=${VM_IP}/24,gw=10.20.20.1
+	qm set $VM_ID --net0 model=virtio,bridge=vmbr1337
+	qm set $VM_ID --ipconfig0 ip=${VM_IP}/24,gw=10.33.0.1
 	qm set $VM_ID --nameserver '157.159.10.12'
 	qm set $VM_ID --ciuser deploy
 	qm set $VM_ID --sshkey ~/.ssh/id_rsa.pub
@@ -27,14 +27,14 @@ cat join_cluster.cmd | head -n 5 | tail -n 1 | tee join_cluster.sh
 
 for i in {2..5}; do
 	VM_ID="100$i"
-	VM_IP="10.20.20.1$i"
+	VM_IP="10.33.0.1$i"
 	if [ "$(qm list | grep $VM_ID | wc -l)" -eq 0 ]; then
 		qm clone 9000 $VM_ID \
 			--name "docker$i" \
 			--description 'Docker $i VM created by bonnetn' \
 			--pool dockerswarm 
-		qm set $VM_ID --net0 model=virtio,bridge=vmbr20
-		qm set $VM_ID --ipconfig0 ip=${VM_IP}/24,gw=10.20.20.1
+		qm set $VM_ID --net0 model=virtio,bridge=vmbr1337
+		qm set $VM_ID --ipconfig0 ip=${VM_IP}/24,gw=10.33.0.1
 		qm set $VM_ID --nameserver '157.159.10.12'
 		qm set $VM_ID --ciuser deploy
 		qm set $VM_ID --sshkey ~/.ssh/id_rsa.pub
@@ -47,14 +47,14 @@ done
 read -p "Press enter to continue when the worker VMs are started"
 
 for i in {2..5}; do
-	VM_IP="10.20.20.1$i"
+	VM_IP="10.33.0.1$i"
 	scp -o StrictHostKeyChecking=no .provision_vm.sh deploy@${VM_IP}:./provision.sh
 	ssh -o StrictHostKeyChecking=no deploy@$VM_IP bash provision.sh
 	scp -o StrictHostKeyChecking=no join_cluster.sh deploy@${VM_IP}:./
 	ssh -o StrictHostKeyChecking=no deploy@$VM_IP sudo bash join_cluster.sh
 done
 
-VM_IP="10.20.20.11"
+VM_IP="10.33.0.11"
 scp -o StrictHostKeyChecking=no .promote_everyone.sh deploy@${VM_IP}:./promote.sh
 ssh -o StrictHostKeyChecking=no deploy@$VM_IP sudo bash promote.sh
 
